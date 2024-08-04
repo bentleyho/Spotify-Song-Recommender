f8dcca7e50e9776a5c0c8455e25aeb0e6eabd700
bentleyho <86305292+bentleyho@users.noreply.github.com>
2024-08-04 15:21:01 -0400

    Working Test

--- app/songrecommender.py
+++ app/songrecommender.py
@@ -16,81 +16,93 @@ spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 token = client_credentials_manager.get_access_token(as_dict=False)
 
+def get_track_id(track_name ="Peaches", artist_name ="Justin Beiber"):
+        results = spotify.search(q=f'track:{track_name} artist:{artist_name}', type='track')
+        items = results['tracks']['items']
+        if items:
+            return items[0]['id']
+        else:
+            return None
 
-def get_track_id(track_name, artist_name):
-    results = spotify.search(q=f'track:{track_name} artist:{artist_name}', type='track')
-    items = results['tracks']['items']
-    if items:
-        return items[0]['id']
+# the weird main conditional says
+# only run the indented code if you are running this file
+# from the command line
+# otherwise if importing from this file, ignore the stuff below
+    
+if __name__ == "__main__":
+    def get_track_id(track_name, artist_name):
+        results = spotify.search(q=f'track:{track_name} artist:{artist_name}', type='track')
+        items = results['tracks']['items']
+        if items:
+            return items[0]['id']
+        else:
+            return None
+
+    u = input("Please type the Song Name:").strip()
+    z = input("Please type the Artist Name:").strip()
+    track_name = u
+    artist_name = z
+    track_id = get_track_id(track_name, artist_name)
+
+    if track_id:
+        print(f'Track ID for "{track_name}" by {artist_name}: {track_id}')
     else:
-        return None
+        print(f'Track "{track_name}" by {artist_name} not found.')
 
-u = input("Please type the Song Name:").strip()
-z = input("Please type the Artist Name:").strip()
-track_name = u
-artist_name = z
-track_id = get_track_id(track_name, artist_name)
+    audio_features = spotify.audio_features(track_id)
 
-if track_id:
-    print(f'Track ID for "{track_name}" by {artist_name}: {track_id}')
-else:
-    print(f'Track "{track_name}" by {artist_name} not found.')
-
-audio_features = spotify.audio_features(track_id)
-
-if audio_features:
-    features = audio_features[0]
-    print(features)
-else:
-    print("Track not found or no features available.")
-
-seed_track = spotify.track(track_id)
-
-seed_track_name = seed_track["name"]
-
-
-recommendations = spotify.recommendations(seed_tracks=[track_id])
-
-if "tracks" in recommendations:
-    recommended_tracks = recommendations["tracks"]
-    if len(recommended_tracks) > 0:
-        print(f"Songs similar to '{seed_track_name}':")
-        for track in recommended_tracks:
-            track_name = track["name"]
-            artist_name = track["artists"][0]["name"]
-            print(f"Track: {track_name} - Artist: {artist_name}")
+    if audio_features:
+        features = audio_features[0]
+        print(features)
     else:
-        print("No recommendations found.")
-else:
-    print("Recommendations not available.")
+        print("Track not found or no features available.")
 
+    seed_track = spotify.track(track_id)
 
-# convert image URL to html
-# credit to: https://towardsdatascience.com/rendering-images-inside-a-pandas-dataframe-3631a4883f60
+    seed_track_name = seed_track["name"]
 
-def img_html(url):
-    return '<img src="'+ url + '" width="50" >'
+    recommendations = spotify.recommendations(seed_tracks=[track_id])
 
-def preview_html(url):
-    if url:
-        return '<a href="'+ url + '" >Listen on Spotify</a>'
+    if "tracks" in recommendations:
+        recommended_tracks = recommendations["tracks"]
+        if len(recommended_tracks) > 0:
+            print(f"Songs similar to '{seed_track_name}':")
+            for track in recommended_tracks:
+                track_name = track["name"]
+                artist_name = track["artists"][0]["name"]
+                print(f"Track: {track_name} - Artist: {artist_name}")
+        else:
+            print("No recommendations found.")
     else:
-        return None
+        print("Recommendations not available.")
 
-records = []
-for index, track in enumerate(recommendations['tracks']): 
-    record = {
-        "index": index,
-        "name": track['name'],
-        "artist": track["artists"][0]["name"],
-        "popularity": track["popularity"],
-        "preview_url": preview_html(track["preview_url"]),
-        "album_art": img_html(track["album"]["images"][0]["url"])
-    }
-    records.append(record)
 
-tracks_df = DataFrame(records)
-tracks_df.head()
+    # convert image URL to html
+    # credit to: https://towardsdatascience.com/rendering-images-inside-a-pandas-dataframe-3631a4883f60
 
-# displaying the dataframe as HTML, with HTML links and images:
-tracks_table = HTML(tracks_df.to_html(escape=False, index=False, formatters=dict(Icon=img_html)))
+    def img_html(url):
+        return '<img src="'+ url + '" width="50" >'
+
+    def preview_html(url):
+        if url:
+            return '<a href="'+ url + '" >Listen on Spotify</a>'
+        else:
+            return None
+
+    records = []
+    for index, track in enumerate(recommendations['tracks']): 
+        record = {
+            "index": index,
+            "name": track['name'],
+            "artist": track["artists"][0]["name"],
+            "popularity": track["popularity"],
+            "preview_url": preview_html(track["preview_url"]),
+            "album_art": img_html(track["album"]["images"][0]["url"])
+        }
+        records.append(record)
+
+    tracks_df = DataFrame(records)
+    tracks_df.head()
+
+    # displaying the dataframe as HTML, with HTML links and images:
+    tracks_table = HTML(tracks_df.to_html(escape=False, index=False, formatters=dict(Icon=img_html)))
--- test/songs_test.py
+++ test/songs_test.py
@@ -1,5 +1,25 @@
-# this is the "test/stocks_test.py" file...
+import pytest
+from app.clientIDSecret import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+from pandas import DataFrame
 
-def test_example():
+client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    assert 2 + 2 == 4
+def get_track_id(track_name, artist_name):
+    results = spotify.search(q=f'track:{track_name} artist:{artist_name}', type='track')
+    items = results['tracks']['items']
+    if items:
+        return items[0]['id']
+    else:
+        return None
+
+def test_get_track_id():
+    # Test with a known track
+    track_id = get_track_id('Off My Face', 'Justin Bieber')
+    assert track_id is not None, "Track ID should not be None for a known track"
+    
+    # Test with a non-existent track
+    track_id = get_track_id('Mocking Bird', 'Justin Beiber')
+    assert track_id is None, "Track ID should be None for a non-existent track"
